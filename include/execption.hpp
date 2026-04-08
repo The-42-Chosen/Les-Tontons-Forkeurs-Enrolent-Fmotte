15fed192f0dbfdf60027a512274e7ea37f7ae06c
Florent <florentm.contact@gmail.com>
2026-04-08 15:23:56 +0200

    Create Class Exeption

---
+++ include/execption.hpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execption.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmotte <fmotte@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/08 13:08:20 by fmotte            #+#    #+#             */
+/*   Updated: 2026/04/08 15:20:17 by fmotte           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "struct.hpp"
+	
+class ExecptionMissSemiColon : public std::exception 
+{
+    public: const char* what() const throw();
+};
+
+class ExecptionMissBrace : public std::exception 
+{
+    public: const char* what() const throw();
+};
+
+class ExecptionWrongArgument : public std::exception 
+{
+    private: std::string _message;
+    public:
+        ExecptionWrongArgument(const std::string& argument);
+        ~ExecptionWrongArgument() throw();
+        const char* what() const throw();
+};
+
+class ExecptionIllegalMethod: public std::exception 
+{
+    private: std::string _message;
+    public:
+        ExecptionIllegalMethod(const std::string& argument);
+        ~ExecptionIllegalMethod() throw();
+        const char* what() const throw();
+};
+
+class ExecptionFailConvertion: public std::exception 
+{
+    private: std::string _message;
+    public:
+        ExecptionFailConvertion(const std::string& argument);
+        ~ExecptionFailConvertion() throw();
+        const char* what() const throw();
+};
---
+++ src/execption.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execption.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmotte <fmotte@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/08 13:08:22 by fmotte            #+#    #+#             */
+/*   Updated: 2026/04/08 15:13:38 by fmotte           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "execption.hpp"
+
+const char* ExecptionMissSemiColon::what() const throw() {return "Error: Miss semi-colon in the config file";}
+
+const char* ExecptionMissBrace::what() const throw() {return "Error: Miss curly brace in the config file";}
+
+ExecptionWrongArgument::ExecptionWrongArgument(const std::string& argument): _message("Error: Cannont interprete this argument: " + argument) {}
+ExecptionWrongArgument::~ExecptionWrongArgument() throw() {}
+const char* ExecptionWrongArgument::what() const throw() {return _message.c_str();}
+
+
+ExecptionIllegalMethod::ExecptionIllegalMethod(const std::string& argument): _message("Error: Illegal method http: " + argument) {}
+ExecptionIllegalMethod::~ExecptionIllegalMethod() throw() {}
+const char* ExecptionIllegalMethod::what() const throw() {return _message.c_str();}
+
+ExecptionFailConvertion::ExecptionFailConvertion(const std::string& argument): _message("Error: Can't convert (" + argument + ") to unsigned int") {}
+ExecptionFailConvertion::~ExecptionFailConvertion() throw() {}
+const char* ExecptionFailConvertion::what() const throw() {return _message.c_str();}
