3f6d584193430f62518b9b350b0205110c2b823f
erpascua <erpascua@student.42.fr>
2026-04-09 14:45:37 +0200

    (chore): Functions on main.cpp moved to utils.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fmotte <fmotte@student.42.fr>              +#+  +:+       +#+        */
+/*   By: erpascua <erpascua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/03 15:24:58 by fmotte            #+#    #+#             */
-/*   Updated: 2026/04/08 13:46:56 by fmotte           ###   ########.fr       */
+/*   Updated: 2026/04/09 14:26:50 by erpascua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,83 +14,7 @@
 # include "server.hpp"
 # include "utils.hpp"
 
-int parse_config_file(char *filename, std::string &content_file)
-{
-    //Reading
-    std::ifstream f(filename);
-    if (!f.is_open())
-    {
-        std::cout << "Error: can't open " << filename << std::endl;
-        return (1);
-    }
-    
-    std::string line;
-    while (std::getline(f, line))
-    {
-        if (!f.eof())
-            line.append("\n");
-        else
-            break;
-        content_file.append(line);
-    }
-    
-    f.close();
-    return (0);
-}
 
-std::vector <std::string> tokenize_string(std::string &content_file)
-{   
-    std::vector <std::string> tokens;
-    std::string delimiters = " \t\n\0";
-    std::string tmp;
-    size_t startPos = 0;
-    size_t endPos = 0;
-
-    while ((endPos = content_file.find_first_of(delimiters, startPos)) != std::string::npos)
-    {
-        if (endPos != startPos)
-        {
-            //Skip comment
-            if (content_file[startPos] == '#')
-            {
-                while (content_file[endPos] != '\n')
-                    ++endPos;
-            }
-            else
-                tokens.push_back(content_file.substr(startPos, endPos - startPos));
-        }
-         
-        startPos = endPos + 1; 
-    }
-
-    if (startPos != content_file.length())
-        tokens.push_back(content_file.substr(startPos));
-
-    
-    for(size_t i = 0; i < tokens.size(); ++i)
-    {
-        if (get_nb_occurence(tokens[i], '"') == 1)
-        {
-            if (i + 1 < tokens.size())
-            {
-                tokens[i].append(" ");
-                tokens[i].append(tokens[i+ 1]);
-                tokens.erase(tokens.begin() + i + 1);
-            }  
-        }
-        if (*(tokens[i].end() - 1) == ';' && tokens[i].size() != 1)
-        {
-            tokens[i] = tokens[i].substr(0, tokens[i].size()-1);
-            tokens.insert(tokens.begin() + i + 1, ";");
-            i++;
-        }
-    }
-    
-    // for(size_t i = 0; i < tokens.size(); ++i)
-    //     std::cout << tokens[i] << "\n";
-        
-    return tokens;
-}
 
 int main(int argc, char **argv)
 {
@@ -108,7 +32,8 @@ int main(int argc, char **argv)
     tokens = tokenize_string(content_file);
     
     Server server;  
-    server.initialisation_webserv(tokens);
+    if (!server.initialisation_webserv(tokens))
+		return (1);
         
     return (0);
 }
