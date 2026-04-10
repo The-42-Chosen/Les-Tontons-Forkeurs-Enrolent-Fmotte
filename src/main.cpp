28c1022044514cafbb14934650c3558482109866
erpascua <erpascua@student.42.fr>
2026-04-10 19:49:29 +0200

    (feat): Pending - Serving added to vector _servers

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,14 @@
 /*   By: erpascua <erpascua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/03 15:24:58 by fmotte            #+#    #+#             */
-/*   Updated: 2026/04/09 14:26:50 by erpascua         ###   ########.fr       */
+/*   Updated: 2026/04/10 14:47:14 by erpascua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "struct.hpp"
 # include "server.hpp"
 # include "utils.hpp"
+# include "colors.hpp"
 
 
 
@@ -34,6 +35,17 @@ int main(int argc, char **argv)
     Server server;  
     if (!server.initialisation_webserv(tokens))
 		return (1);
+    
+    // TEST PRINT SERVERS
+    std::cout << RED << "\n=== RÉSUMÉ DES SERVEURS PARSÉS ===\n"  << RESET;
+    for (size_t i = 0; i < server.get_servers_count(); i++)
+    {
+        Server* srv = server.get_server(i);
+        if (srv != NULL)
+            std::cout << "Serveur " << (i + 1) << " ajouté au vector\n";
+    }
+    std::cout << "Total: " << server.get_servers_count() << " serveur(s)\n";
+    std::cout << "==================================\n\n";
         
     return (0);
 }
