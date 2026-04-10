3cf7f4a9459188aac79d237937c9bc644c6225ed
erpascua <erpascua@student.42.fr>
2026-04-10 19:48:05 +0200

    (chore): Clang-Format applied to Exception

--- include/execption.hpp
+++ include/execption.hpp
@@ -3,57 +3,69 @@
 /*                                                        :::      ::::::::   */
 /*   execption.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fmotte <fmotte@student.42.fr>              +#+  +:+       +#+        */
+/*   By: erpascua <erpascua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/08 13:08:20 by fmotte            #+#    #+#             */
-/*   Updated: 2026/04/08 16:28:15 by fmotte           ###   ########.fr       */
+/*   Updated: 2026/04/10 14:43:41 by erpascua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "struct.hpp"
-	
-class ExecptionMissSemiColon : public std::exception 
+#pragma once
+
+#include "struct.hpp"
+
+class ExecptionMissSemiColon : public std::exception
 {
-    public: const char* what() const throw();
+  public:
+    const char *what() const throw();
 };
 
-class ExecptionMissBrace : public std::exception 
+class ExecptionMissBrace : public std::exception
 {
-    public: const char* what() const throw();
+  public:
+    const char *what() const throw();
 };
 
-class ExecptionWrongArgument : public std::exception 
+class ExecptionWrongArgument : public std::exception
 {
-    private: std::string _message;
-    public:
-        ExecptionWrongArgument(const std::string& argument);
-        ~ExecptionWrongArgument() throw();
-        const char* what() const throw();
+  private:
+    std::string _message;
+
+  public:
+    ExecptionWrongArgument(const std::string &argument);
+    ~ExecptionWrongArgument() throw();
+    const char *what() const throw();
 };
 
-class ExecptionIllegalMethod: public std::exception 
+class ExecptionIllegalMethod : public std::exception
 {
-    private: std::string _message;
-    public:
-        ExecptionIllegalMethod(const std::string& argument);
-        ~ExecptionIllegalMethod() throw();
-        const char* what() const throw();
+  private:
+    std::string _message;
+
+  public:
+    ExecptionIllegalMethod(const std::string &argument);
+    ~ExecptionIllegalMethod() throw();
+    const char *what() const throw();
 };
 
-class ExecptionFailConvertion: public std::exception 
+class ExecptionFailConvertion : public std::exception
 {
-    private: std::string _message;
-    public:
-        ExecptionFailConvertion(const std::string& argument);
-        ~ExecptionFailConvertion() throw();
-        const char* what() const throw();
+  private:
+    std::string _message;
+
+  public:
+    ExecptionFailConvertion(const std::string &argument);
+    ~ExecptionFailConvertion() throw();
+    const char *what() const throw();
 };
 
-class ExecptionMissElement: public std::exception 
+class ExecptionMissElement : public std::exception
 {
-    private: std::string _message;
-    public:
-        ExecptionMissElement(const std::string& argument);
-        ~ExecptionMissElement() throw();
-        const char* what() const throw();
+  private:
+    std::string _message;
+
+  public:
+    ExecptionMissElement(const std::string &argument);
+    ~ExecptionMissElement() throw();
+    const char *what() const throw();
 };
