e4c79da87e09b456cbcb3f09dd77a38bb79680fd
Florent <florentm.contact@gmail.com>
2026-04-15 19:36:03 +0200

    (hook): Clang-format applied

--- include/utils_connection.hpp
+++ include/utils_connection.hpp
@@ -12,8 +12,8 @@
 
 #pragma once
 
-#include "struct.hpp"
 #include "Client.hpp"
+#include "struct.hpp"
 
 extern int stop_webserv;
 
@@ -22,5 +22,5 @@ void init_signal(struct sigaction &sa);
 int set_nonblocking(int fd);
 
 sockaddr_in create_socket_adrress(std::string ip_address, unsigned int port_number);
-void add_socket_to_event(int epoll_fd, int socket_fd, Client* client);
+void add_socket_to_event(int epoll_fd, int socket_fd, Client *client);
 int create_server_socket(std::string ip_address, unsigned int port_number, unsigned int max_client);
