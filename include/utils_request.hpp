ff47c70d5fc1f6c7ab937093fa06608adbed78ef
Florent <florentm.contact@gmail.com>
2026-04-22 17:36:41 +0200

    (fix): Executable repaired & Comparaison not depending on ASCII order only

--- include/utils_request.hpp
+++ include/utils_request.hpp
@@ -6,7 +6,7 @@
 /*   By: fmotte <fmotte@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/21 13:53:22 by fmotte            #+#    #+#             */
-/*   Updated: 2026/04/21 13:53:44 by fmotte           ###   ########.fr       */
+/*   Updated: 2026/04/22 16:55:25 by fmotte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,5 @@
 
 #include "struct.hpp"
 
+int computeRemainingCost(const std::string &string, size_t min_len, int score);
 int longestPrefixMatch(std::string string1, std::string string2);
--- src/utils_request.cpp
+++ src/utils_request.cpp
@@ -6,53 +6,65 @@
 /*   By: fmotte <fmotte@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/21 13:53:46 by fmotte            #+#    #+#             */
-/*   Updated: 2026/04/22 11:15:47 by fmotte           ###   ########.fr       */
+/*   Updated: 2026/04/22 17:24:26 by fmotte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils_request.hpp"
 
-int longestPrefixMatch(std::string string1, std::string string2)
-{
-    size_t i;
-    int score = 0;
 
+int computeRemainingCost(const std::string &string, size_t min_len, int score)
+{
+    for (size_t i = min_len; i < string.size(); ++i)
+    {
+        if (islower(string[i]))
+            score += std::abs(string[i] - 'a');
+        else if (isupper(string[i]))
+            score += std::abs(string[i] - 'A'); 
+        else
+            score += string[i];
+    }
+    return score;
+}
+
+int longestPrefixMatch(std::string uri, std::string location)
+{
+    int score = 0;
+    
     std::string sub_string;
     std::vector<std::string> token_str1;
     std::vector<std::string> token_str2;
 
-    std::stringstream iss1(string1);
+    std::stringstream iss1(uri);
     while (getline(iss1, sub_string, '/'))
         token_str1.push_back(sub_string);
 
-    std::stringstream iss2(string2);
+    std::stringstream iss2(location);
     while (getline(iss2, sub_string, '/'))
         token_str2.push_back(sub_string);
 
-    while (!token_str1.empty() || !token_str2.empty())
+    size_t i = 0, j = 0;
+    while (i < token_str1.size() || j < token_str2.size())
     {
-        const std::string s1 = token_str1.empty() ? "" : token_str1.front();
-        const std::string s2 = token_str2.empty() ? "" : token_str2.front();
-
+        if (token_str1.size() == i +1 && token_str1[i].find('.') != std::string::npos)
+        {
+            ++i;
+            continue;
+        }
+            
+        const std::string& s1 = (i < token_str1.size()) ? token_str1[i] : "";
+        const std::string& s2 = (j < token_str2.size()) ? token_str2[j] : "";
+ 
         size_t min_len = std::min(s1.size(), s2.size());
 
-        // Comparaison caractère par caractère
         for (size_t i = 0; i < min_len; ++i)
             score += std::abs(s1[i] - s2[i]);
 
-        // Ajouter les caractères restants
-        for (size_t i = min_len; i < s1.size(); ++i)
-            score += s1[i];
-
-        for (size_t i = min_len; i < s2.size(); ++i)
-            score += s2[i];
-
-        if (!token_str1.empty())
-            token_str1.erase(token_str1.begin());
-        if (!token_str2.empty())
-            token_str2.erase(token_str2.begin());
+        score += computeRemainingCost(s1, min_len, score);
+        score += computeRemainingCost(s2, min_len, score);
+            
+        ++i;
+        ++j;
     }
-
-    std::cout << "Score: " << score << "\n";
     return score;
 }
