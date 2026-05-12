56fed71877f13f9feabcbcef229fa31cb4a468f2
erpascua <erpascua@student.42.fr>
2026-05-12 20:15:03 +0200

    (hook): Clang-format applied

--- src/http/HttpResponse.cpp
+++ src/http/HttpResponse.cpp
@@ -18,8 +18,8 @@
 
 HttpResponse::HttpResponse(int code, Body &body, const std::string &contentType) : _statusCode(code), _body(body)
 {
-	if (!contentType.empty())
-		_headers["content-type"] = contentType;
+    if (!contentType.empty())
+        _headers["content-type"] = contentType;
 }
 
 HttpResponse::~HttpResponse()
@@ -32,30 +32,29 @@ HttpResponse::~HttpResponse()
 
 void HttpResponse::addHeader(const std::string &key, const std::string &val)
 {
-	_headers[key] = val;
+    _headers[key] = val;
 }
 
 std::string HttpResponse::build() const
 {
-	std::stringstream ss;
+    std::stringstream ss;
     ss << _statusCode;
     std::string response = "HTTP/1.1 " + ss.str() + " " + _statusMessage + "\r\n";
 
-	for(Header::const_iterator it = _headers.begin(); it != _headers.end(); it++)
-		response += it->first + ": " + it->second + "\r\n";
-	response += "\r\n";
-	response += std::string(_body.begin(), _body.end());
-	
-	return (response);
+    for (Header::const_iterator it = _headers.begin(); it != _headers.end(); it++)
+        response += it->first + ": " + it->second + "\r\n";
+    response += "\r\n";
+    response += std::string(_body.begin(), _body.end());
+
+    return (response);
 }
 
 void HttpResponse::send(int fd) const
 {
-	std::string headers = build(); // build() sans le body
+    std::string headers = build(); // build() sans le body
     ::send(fd, headers.c_str(), headers.size(), 0);
-      if (!_body.empty())
-          ::send(fd, _body.data(), _body.size(), 0);
-
+    if (!_body.empty())
+        ::send(fd, _body.data(), _body.size(), 0);
 }
 
 // =====================
@@ -64,7 +63,6 @@ void HttpResponse::send(int fd) const
 
 HttpResponse HttpResponse::makeFile(const std::string &path)
 {
-	
 }
 
 HttpResponse HttpResponse::makeError(int code, const std::string &customPage = "")
