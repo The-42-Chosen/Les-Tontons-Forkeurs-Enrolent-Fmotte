09147cf75edc9bcd223567cf2300f390f2068332
Florent <florentm.contact@gmail.com>
2026-05-29 16:18:21 +0200

    (feat): Create different class Response for the Server

--- include/http/HttpResponse.hpp
+++ include/http/HttpResponse.hpp
@@ -6,7 +6,7 @@
 /*   By: fmotte <fmotte@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/12 11:56:16 by erpascua          #+#    #+#             */
-/*   Updated: 2026/05/24 18:55:25 by fmotte           ###   ########.fr       */
+/*   Updated: 2026/05/29 15:49:36 by fmotte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,9 @@ class Request;
 class HttpResponse
 {
   	private:
+	    // =====================
+        // ==    Attributs    ==
+        // =====================
 		std::string _responseContent;
 		Request *_request;
 		
@@ -31,20 +34,22 @@ class HttpResponse
 		HttpResponse(Request *Request);
 		~HttpResponse();
 		
+		
 		// =====================
 		// ==     Getters     ==
 		// =====================
 		std::string getResponseContent();
 		void setResponseContent(std::string responseContent);
+		void addResponseContent(std::string responseContent);
 		Request *getRequest(void) const;
         void setRequest(Request *Request);
 		
 		// =====================
 		// == 	  Methods	  ==
 		// =====================
-		void initialisationHttpResponse(std::string payload);
-		void handleError();
-		void handleRedirection();
-		void handleCorrect(HttpMethod method, std::string payload);
+		void initialisationHttpResponse();
+		void handleError(int statusCode);
+		void handleRedirection(int statusCode);
+		void handleCorrect(int statusCode);
 		void sendToClient();
 };
