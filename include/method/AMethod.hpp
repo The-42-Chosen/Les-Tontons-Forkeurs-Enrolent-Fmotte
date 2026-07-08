ae8b98a79f36450a1d2a54f11e0f5c3267c0c56f
Florent <florentm.contact@gmail.com>
2026-07-08 22:08:50 +0200

    (chore): Remove CGI from class Amethod

--- include/method/AMethod.hpp
+++ include/method/AMethod.hpp
@@ -6,7 +6,7 @@
 /*   By: fmotte <fmotte@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/12 12:50:28 by fmotte            #+#    #+#             */
-/*   Updated: 2026/07/06 05:39:47 by fmotte           ###   ########.fr       */
+/*   Updated: 2026/07/08 21:45:32 by fmotte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,11 +50,4 @@ class AMethod
     // == 	  Member	  ==
     // =====================
     virtual std::string applyMethod(Location *location) = 0;
-
-    // =====================
-    // ==       CGI       ==
-    // =====================
-    std::string applyCGI(std::string path, const std::string &interpreter);
-    void manage_pipe(std::string path, int pipe_out[2], int pipe_in[2], const std::string &interpreter);
-    void forwardCgiHeaders(const std::string &headerBlock);
 };
